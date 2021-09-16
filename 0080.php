44d9c13aba5b7ec17ec68e40fecf320ab2ed66c2
frankinguerrero <82236772+frankinguerrero@users.noreply.github.com>
2021-09-15 22:21:00 -0500

    Add files via upload

--- 0080.php
+++ 0080.php
@@ -23,19 +23,82 @@
 <form action="Info.php" method="post">;
 <center><p>Nombre:<input type="text" name="Nombre" /></p></center>
 <center><p>Apellido:<input type="text" name="Apellido" /></p></center>
-<center><p>CC/TI:<input type="text" name="CC_TI" /></p></center>
+<center>
+<label for"">CC_TI:</label>
+<select name="CC_TI">
+<option  value=""></option>
+<option  value="ti">Tarjeta de identidad</option>
+<option  value="Cedula">Cedula</option>
+</select>
+</center>
+
 <center><p>N_Identificacion:<input type="text" name="N_Identificacion" /></p></center>
 <center><p>Correo:<input type="text" name="Correo" /></p></center>
 <center><p>Fecha de Contacto:<input type="date" name="Fecha_Contacto"/></p></center>
-<center><p>Pais:<input type="text" name="Pais" /></p></center>
-<center><p>Ciudad:<input type="text" name="Ciudad" /></p></center>
+<center>
+<label for"">Pais:</label>
+<select name="Pais">
+<option  value=""></option>
+<option  value="Colombia">Colombia</option>
+<option  value="Argentina">Argentina</option>
+<option  value="Chile">Chile</option>
+<option  value="Peru">Peru</option>
+</select>
+</center>
+<br>
+<center>
+<label for"">Ciudad:</label>
+<select name="Ciudad">
+<option  value=""></option>
+<option  value="Bogota">Bogota</option>
+<option  value="Cali">Cali</option>
+<option  value="Cartagena">Cartagena</option>
+<option  value="Barranquilla">Barranquilla</option>
+<option  value="Medellin">Medellin</option>
+<option  value="Leticia">Leticia</option>
+<option  value="Arauca">Arauca</option>
+<option  value="Barranquilla">Tunja</option>
+<option  value="Bogota">Manizales</option>
+<option  value="Cali">Florencia</option>
+<option  value="Cartagena">Yopal</option>
+<option  value="Barranquilla">Popayan</option>
+<option  value="Valledupar">Valledupar</option>
+<option  value="Quibdo">Quibdo</option>
+<option  value="Monteria">Monteria</option>
+<option  value="Inirida">Inirida</option>
+<option  value="San Jose del Guaviare">San Jose del Guaviare</option>
+<option  value="Neiva">Neiva</option>
+<option  value="Rioacha">Rioacha</option>
+<option  value="Santa Marta">Santa Marta</option>
+<option  value="Villavicencio">Villavicencio</option>
+<option  value="San Juan de Pasto">San Juan de Pasto</option>
+<option  value="Pasto">Pasto</option>
+<option  value="Cucuta">Cucuta</option>
+<option  value="Mocoa">Mocoa</option>
+<option  value="Pereira">Pereira</option>
+<option  value="San Andres">San Andres</option>
+<option  value="Bucaramanga">Bucaramanga</option>
+<option  value="Sincelejo">Sincelejo</option>
+<option  value="Ibague">Ibague</option>
+<option  value="Mitu">Mitu</option>
+<option  value="Puerto Carreño">Puerto Carreño</option>
+</select>
+</center>
+
+
+
+
 <center><p>Direccion:<input type="text" name="Direccion" /></p></center>
 <center><p>Motivo de contacto:<input type="text" name="Motivo_contacto" /></p></center>
 <center><p>Fecha de Incidencia:<input type="date" name="Fecha_Incidencia" /></p></center>
 <center><input type="submit"  value="Enviar"></center>
+<!--En las lineas anteriores se declararo ,la informacion que queremos que el cliente nos proporcione -->
 </form>
 <center>
 <img src="1.jpg">
 </center>
 </body>
 </html>
+
+
+
--- Info.php
+++ Info.php
@@ -26,5 +26,116 @@ echo "El pais donde se ocasiono la incidenia es: $Pais <br>";
 echo "La ciudad es: $Ciudad <br>";
 echo "La direccion del cliente es: $Direccion <br>";
 echo "El motivo del contacto es: $Motivo_contacto <br>";
-echo "La fecha de la incidencica fue el : $Fecha_Incidencia ";
+echo "La fecha de la incidencica fue el : $Fecha_Incidencia <br> <br>";
+?>
+<strong>Punto de atencion disponible</strong>
+<br>
+<?php
+if ($Ciudad == 'Bogota'){
+echo " cuenta con un punto fisco ,para atender sus peticiones ";
+ } elseif ($Ciudad == 'Cali'){
+  echo "No cuenta con un punto de Atencion al cliente";
+ }elseif ($Ciudad == 'Cartagena'){
+echo "cuenta con un punto fisco ,para atender sus peticiones";
+} elseif ($Ciudad == 'Barranquilla'){
+echo "cuenta con un punto fisco ,para atender sus peticiones";
+ }elseif ($Ciudad == 'Medellin'){
+echo "cuenta con un punto fisco ,para atender sus peticiones";
+}elseif ($Ciudad == 'Leticia'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Arauca'){
+ echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Tunja'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Manizales'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Florencia'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Yopal'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Popayan'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Valledupar'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Quibdo'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Monteria'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Inirida'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'San Jose del Guaviare'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Neiva'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Rioacha'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Santa Marta'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Villavicencio'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'San Juan de Pasto'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Pasto'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Cucuta'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Mocoa'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Pereira'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'San Andres'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Bucaramanga'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Sincelejo'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Ibague'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Mitu'){
+echo "No cuenta con un punto de Atencion al cliente";
+}elseif ($Ciudad == 'Puerto Carreño'){
+echo "No cuenta con un punto de Atencion al cliente";
+}else {
+  echo "No se encuentra en el punto indicado";
+}
+?>
+<br>
+<br>
+<strong>Eres apto para hacer la solisitud?</strong><br>
+<?php
+if ($CC_TI == 'Cedula'){
+    echo "Puedes continuar con el proceso";
+}elseif ($CC_TI == 'ti'){
+    echo "No eres apto para realizar una queja";
+}else {
+    echo "No puedes continuar con el proceso ,porque no seleccionaste ningun campo";
+  }
+?>
+<br>
+<br>
+<strong>Tiendas disponibles</strong>
+<br>
+<?php
+if ($Pais == 'Colombia'){
+    echo "Colombia cuenta con 53 tiendas, donde puedes dirigirte para realizar cualquier compra o reclamo";
+}elseif ($Pais == 'Argentina'){
+    echo "Argentina cuenta con 78 tiendas, donde puedes dirigirte para realizar cualquier compra o reclamo";
+}elseif ($Pais == 'Chile'){
+    echo "Chile cuenta con 62 tiendas, donde puedes dirigirte para realizar cualquier compra o reclamo";
+}elseif ($Pais == 'Peru'){
+    echo "Peru cuenta con 31 tiendas, donde puedes dirigirte para realizar cualquier compra o reclamo";
+}else {
+    echo "No se encontraron  tiendas disponibles";
+  }
+?>
+
+<br>
+<br>
+<strong>Tiempo de respuesta</strong>
+<br>
+<?php
+$Fecha_contacto = date("n");
+$num1 =1; 
+echo "Recibira una respuesta, a no mas tardar en el mes";
+echo  date("n")+ $num1; 
 ?>
